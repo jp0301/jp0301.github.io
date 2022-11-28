932c8092824f9ffadaffa930e17f323092cae535
jp0301 <jp0301@naver.com>
2022-11-28 20:56:09 +0900

    Rendering Error #1

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withContentlayer({
   reactStrictMode: true,
   images: {
     loader: 'akamai',
-    path: '/',
+    path: 'https://jp0301.github.io/jp0301.github.io',
   },
   assetPrefix: !debug ? `/${repository}/` : "",
   eslint: {
